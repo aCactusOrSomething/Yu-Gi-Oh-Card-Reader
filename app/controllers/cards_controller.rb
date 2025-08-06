79cb4884b72cd7736355f69ef70a415d031d482d
Hud Miller <whudsonmiller@gmail.com>
2025-08-06 16:10:30 -0500

    advanced search functionality

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -12,16 +12,34 @@ class CardsController < ApplicationController
   #  render :index
   #end
 
-  #TODO: replace index with this.
   def index
     @cards = Card.all
-    @term = ''
-    @term = params[:query].present? ? params[:query] : ''
-    clean_term =  @term.upcase.gsub(/[^\x00-\x7F]/, ' ')
-    
-    @cards = @cards.where(['name_searchable LIKE ?', ('%' + params[:query].upcase.gsub(/[^\x00-\x7F]/, ' ') + '%')]) if params[:query].present?
-      
-    render :index  
+    @term = params[:name_searchable]
+
+    params.each do |key, value|
+      if not value == nil and not key == "commit" and not key == "controller" and not key == "action" and not value == 0 and not value == ''
+        new_cards = []
+        type = Card.column_for_attribute(key).type
+        puts "#{key} is #{type.to_s}"
+        if type.to_s == "string"
+          clean_term =  value.upcase.gsub(/[^\x00-\x7F]/, ' ')
+          for card in @cards
+            clean_target = card[key].upcase.gsub(/[^\x00-\x7F]/, ' ')
+            if clean_target.include? clean_term
+              new_cards.push(card)
+            end
+          end
+        elsif type.to_s == "integer"
+          for card in @cards
+            if card[key] == value.to_i
+              new_cards.push(card)
+            end
+          end
+        end
+        @cards = new_cards
+      end
+    end
+    render :index
   end
 
   def show
