a8eb7bae50f2f42efe35b7fdb797164c2976b0ca
Hud Miller <whudsonmiller@gmail.com>
2025-08-06 15:14:28 -0500

    refactored search controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,15 +1,27 @@
 class CardsController < ApplicationController
   
+  #def index
+  #  @term = params[:query]
+  #  clean_term =  @term.upcase.gsub(/[^\x00-\x7F]/, ' ')
+  #  @cards = []
+  #  for card in Card.all
+  #    if card.name_searchable.include? clean_term
+  #      @cards.push(card)
+  #    end
+  #  end
+  #  render :index
+  #end
+
+  #TODO: replace index with this.
   def index
-    @term = params[:query]
+    @cards = Card.all
+    @term = ''
+    @term = params[:query].present? ? params[:query] : ''
     clean_term =  @term.upcase.gsub(/[^\x00-\x7F]/, ' ')
-    @cards = []
-    for card in Card.all
-      if card.name_searchable.include? clean_term
-        @cards.push(card)
-      end
-    end
-    render :index
+    
+    @cards = @cards.where(['name_searchable LIKE ?', ('%' + params[:query].upcase.gsub(/[^\x00-\x7F]/, ' ') + '%')]) if params[:query].present?
+      
+    render :index  
   end
 
   def show
